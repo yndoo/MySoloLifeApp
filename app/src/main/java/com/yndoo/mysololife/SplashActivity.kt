a4b52eb7f355b4bfadf153a507db9f7f29e2a81e
yndoo <yndoo2k@gmail.com>
2023-09-25 21:59:11 +0900

    Initial commit

---
+++ app/src/main/java/com/yndoo/mysololife/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.yndoo.mysololife
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.yndoo.mysololife.auth.IntroActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, IntroActivity::class.java))
+            finish()
+        }, 3000)
+    }
+}
---
+++ app/src/main/java/com/yndoo/mysololife/auth/IntroActivity.kt
@@ -0,0 +1,12 @@
+package com.yndoo.mysololife.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yndoo.mysololife.R
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+    }
+}
